9e25cd46da64d637e61016ca6adcafe1d204a56f
Tatsiana Salikayeva <t.salikayeva@gmail.com>
2021-05-09 01:06:10 +0200

    Added action to Step1 form

--- src/Components/Step1.tsx
+++ src/Components/Step1.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useEffect } from 'react';
+import React, {FunctionComponent, ChangeEvent, useCallback} from 'react';
 import {
     Link
 } from "react-router-dom";
@@ -6,27 +6,33 @@ import Page from "./Page";
 import {StepProps} from "./App";
 
 
+const Step1: FunctionComponent<StepProps> = (props) => {
+    const {formContent, setFormContent} = props;
 
-const Step1:FunctionComponent<StepProps> = (props) => {
-
-    useEffect(() => {
-
+    const onInputChange = useCallback((e: ChangeEvent<HTMLInputElement>, type: string) => {
+        const obj = {
+            ...formContent,
+            [type]: e.target.value
+        };
+        setFormContent(obj);
     }, []);
 
     return (
-       <Page title={"Step1"}>
-           <div className="input-item">
-               <label>Name</label>
-               <input type='text' placeholder="Input Your Name here..."/>
-           </div>
-           <div className="input-item">
-               <label>Email</label>
-               <input type="email" placeholder="Input Your email here..."/>
-           </div>
-           <div className="input-item">
-               <Link to="/step/2"><button style={{float: 'right'}}>Next Step</button></Link>
-           </div>
-       </Page>
+        <Page title={"Step1"}>
+            <div className="input-item">
+                <label>Name</label>
+                <input type='text' value={formContent.name} placeholder="Input Your Name here..." onChange={(e) => onInputChange(e, 'name')}/>
+            </div>
+            <div className="input-item">
+                <label>Email</label>
+                <input type="email" value={formContent.email} placeholder="Input Your email here..." onChange={(e) => onInputChange(e, 'email')}/>
+            </div>
+            <div className="input-item">
+                <Link to="/step/2">
+                    <button style={{float: 'right'}}>Next Step</button>
+                </Link>
+            </div>
+        </Page>
     );
 };
 
